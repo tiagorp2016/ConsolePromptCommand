2df1d9da41443a8a2965212e38c21737f85018f4
TIAGO RODRIGUES PEREIRA <tiago.r.p@hotmail.com>
2017-12-14 11:43:51 -0200

    Projeto 2

---
+++ TesteGitHub2/Pessoa.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TesteGitHub2
+{
+    public class Pessoa
+    {
+        public Pessoa(string nome)
+        {
+            Nome = nome;
+        }
+
+        public int Id { get; private set; }
+        public string Nome { get; private set; }
+    }
+}
